16fba15479710ce12c233c8672f03205ed3cc860
ebibi123 <elad260195@gmail.com>
2024-01-02 23:24:12 +0200

    yawaradii

--- stage0/Program3572.cs
+++ stage0/Program3572.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World1!");
             Welcome3572();
             Welcome2499();
             Console.ReadKey();
