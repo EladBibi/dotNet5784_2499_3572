c0f6bdc45ff4fabfa5a3f497548f59a82e4930d8
ebibi123 <elad260195@gmail.com>
2024-01-03 00:19:05 +0200

    now merge

--- Stage0/Program.cs
+++ Stage0/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace Stage0
+﻿namespace targil0
 {
-    internal class Program
+    partial class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            int b = 6796;
+            Console.WriteLine("Hello123, World11!");
+            Welcome3572();
+            Welcome2499();
+            Console.ReadKey();
+        }
+        static partial void Welcome2499();
+
+        private static void Welcome3572()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine()!;
+            Console.WriteLine("{0}, welcome to my first console application", userName);
         }
     }
 }
+
