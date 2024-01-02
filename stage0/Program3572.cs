20c8f2edb2865c3cd459e8f53c1c8202935cbb54
ebibi123 <elad260195@gmail.com>
2024-01-02 23:23:01 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program3572.cs
@@ -0,0 +1,22 @@
+﻿namespace targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Welcome3572();
+            Welcome2499();
+            Console.ReadKey();
+        }
+        static partial void Welcome2499();
+
+        private static void Welcome3572()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine()!;
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
+
