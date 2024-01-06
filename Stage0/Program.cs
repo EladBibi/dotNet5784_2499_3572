c756afb434874831a60623b60461323c2dafb45b
ebibi123 <elad260195@gmail.com>
2024-01-06 23:36:31 +0200

    sasa

--- Stage0/Program.cs
+++ Stage0/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello12345678910, World11!");
+            Console.WriteLine("Hello1234567891011, World11!");
 
             Welcome3572();
             Welcome2499();
