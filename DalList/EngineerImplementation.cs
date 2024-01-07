55fc78fe4e9ce2e89233d8e0c28e67f836c6834b
ebibi123 <elad260195@gmail.com>
2024-01-07 23:08:47 +0200

    23:08

---
+++ DalList/EngineerImplementation.cs
@@ -0,0 +1,34 @@
+﻿using DO;
+using DalApi;
+
+
+namespace Dal
+{
+    public class EngineerImplementation : IEngineer
+    {
+        public int Create(Engineer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Engineer? Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Engineer> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Engineer item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
