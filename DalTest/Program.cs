3f4c7898bbe062f1c2a1539301687765f5b99256
ebibi123 <elad260195@gmail.com>
2024-01-09 14:23:19 +0200

    update again

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -8,7 +8,7 @@ namespace DalTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World");
 
             MyStaticClass.myStaticVariable = 78;
 
